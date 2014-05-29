4d4b085d43a32a57b4da3d2ea54b0b4d45bae3b4
yale89 <yair.ale89@gmail.com>
2014-05-28 23:03:44 -0300

    Chamullo Yair

--- GestionHoras.cpp
+++ GestionHoras.cpp
@@ -1,16 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <string.h>
 
-
-
-struct{
-}usuario;
-
-#<include conio.h>
-
-
-int main (){
+typedef struct {
+	int cod;
+	char nombre[12];
+	char contrasenia[12];
 	
-	printf ("Mariano topu, creamos un reeeeeeeepooooo");
+}TRegUsuarios;
 
+//Variables Globales
+TRegUsuarios tablaUsuarios[5000];
+
+FILE * fusuarios;
+int cant = 0;
+
+//Funciones
+
+void menu_ppal (){
+	system ("cls");
+	cant = cant +1;
+	printf("------------------------GESTION HORAS--------------------------------------------------------\n");
+	printf ("NOMBRE DE USUARIO:  \n");
+	scanf ("%r",&tablaUsuarios[cant].nombre);
+	printf ("CONTRASEÑA: ");
+	scanf ("%r",&tablaUsuarios[cant].contrasenia);	
+	
+	//guardo en el archivo 
+	fusuarios = fopen("usuarios.dat","a+");
+    fwrite(&tablaUsuarios,sizeof(TRegUsuarios),cant,fusuarios);
+	fclose(fusuarios);
+	
 }
 
+//Main
+
+int main(){
+	
+	int op=0;	
+	
+	menu_ppal();
+	
+	return 0;
+	
+}
